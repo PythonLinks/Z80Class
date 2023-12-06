cf549b032cd3b263031cf14c52f894e419e9e64d
Your Name <you@example.com>
2023-12-06 07:21:33 +0100

    Initial commit.

---
+++ ex1.v
@@ -0,0 +1,131 @@
+//-----------------------------------------------------------------------------
+//
+// Description : Z80 Test unit
+//
+//-----------------------------------------------------------------------------
+`default_nettype none
+
+`timescale 1ns / 100ps
+
+module EX1_tb;
+
+//Internal signals declarations:
+reg CLK;
+reg nCLR;
+reg CE;
+wire [15:0]ADDR;
+//reg [7:0]DI;
+wire [7:0] DQ;
+wire nRD;
+wire nWR;
+wire nMREQ;
+wire nIORQ;
+wire nM1;
+wire nWAIT;
+reg nINT;
+wire [2:0] TS;
+
+// Unit Under Test port map
+TV80_CPU
+	#(.RESET_VECTOR(16'h0000))
+CPU_0 (
+	.CLK(CLK),
+	.nCLR(nCLR),
+	.CE(CE),
+	.ADDR(ADDR),
+	.DQ(DQ),
+	.nRD(nRD),
+	.nWR(nWR),
+	.nMREQ(nMREQ),
+	.nIORQ(nIORQ),
+	.nINT(nINT),
+	.nNMI(1'b1),
+	.nBUSRQ(1'b1),
+	.nBUSAK(),
+	.nWAIT(nWAIT),
+	.nM1(nM1),
+	.TS(TS));
+
+wire RAM_4K_CS = ~(~nMREQ & ~ADDR[15] & ~ADDR[14] & ~ADDR[13] & ~ADDR[12]);
+
+RAM_4K M0(
+	.A(ADDR[11:0]),
+	.nOE(nRD),
+	.nWE(nWR),
+	.nCS(RAM_4K_CS),
+	.DQ(DQ));
+
+initial
+  $display("ADDR    DQ     A   I     PC DIH DIL 2 SUM ");
+   
+//Print out registers content at beginning of machine cycle
+//always @(negedge nM1) begin
+always @(*) begin   
+   $display (ADDR, " ", 
+     DQ, "  ",
+     CPU_0.CPU.A, " ",
+     CPU_0.CPU.I, " ", 	     
+     CPU_0.CPU.PC, " ", 	     
+     CPU_0.CPU.RegDIH, " ",
+     CPU_0.CPU.RegDIL, " ", 	     	     	     
+
+     M0.MEM[12'h11], " ", 
+     M0.MEM[12'h12]	     
+);
+end
+
+
+
+   
+//Stop simulation when IO write -> IO:0x80 = 0xFF
+always @(posedge CLK) begin  //WAS FF not )0F
+   if ((ADDR == 8'hFF) && //WHEN THE ADDRESS IS 0xFF 
+       (nIORQ ==0)  &&   // AND THERE IS AN IO REQUEST
+       (nWR ==0))       // AND THERE IS A WRITE REQUEST
+     //$display("RESULT = ", M0.MEM[18]);
+   
+     $finish;
+end
+
+   
+initial	begin
+	CLK = 1'b0;
+	forever #5 CLK = ~CLK;
+end
+
+assign nWAIT = 1'b1; //No WAIT - Run with full speed
+
+initial begin
+	CE = 1'b1;
+	nCLR = 1'b0;
+	nINT = 1'b1;
+	WAIT(20);
+	nCLR = 1'b1;
+	WAIT(800);
+	$display("@%d: Simulation completed.", $time);
+	$finish;
+end
+
+initial begin
+	$dumpfile("ex1.vcd");
+	$dumpvars(0, EX1_tb);
+	$dumpon;
+end
+
+//---------------------------------------------------------
+//  Included program - use memory tasks to prepare the program
+//---------------------------------------------------------
+
+//`include "../tv80_lib/tv80_mul.v"
+   
+task WAIT;
+input [31:0] dy;
+begin
+	repeat(dy)
+	@(negedge CLK);
+end
+endtask
+
+endmodule
+
+  
---
+++ mem.v
@@ -0,0 +1,129 @@
+`default_nettype none
+
+`timescale 1ns/100ps
+
+   
+module RAM_4K(A, nOE, nWE, nCS, DQ);
+
+input [11:0] A;
+input nOE, nWE, nCS;
+inout [7:0] DQ;
+wire [ 7:0] result;      
+reg [7:0] MEM [4095:0];
+
+
+//TODO: Implement 4kB RAM with task for loading a content using semi assembler
+// start with no values.
+   wire   show;
+   assign show = (~nCS & ~nOE & nWE) ;
+  
+   assign DQ = show ? result : 8'hzz;
+   assign result = MEM[A] ;
+   
+always @(nOE or nWE or DQ)
+    if (!nCS & !nWE)
+	MEM[A] = DQ;
+          
+   
+  
+//---------------------------------------------------------
+// Modelling tasks
+//---------------------------------------------------------
+//WAS integer   
+reg [6:0] label_id;
+reg [11:0] wr_addr;
+   
+   task WR_MEM(   input [7:0] DATA);
+ /* write a byte to the memory */
+   MEM [wr_addr] = DATA;
+endtask
+
+task WR_MEM_W(input [15:0] DATA); /* Write word to the memory array */
+   begin
+   MEM [wr_addr]        = DATA[7:0];
+   MEM [wr_addr + 1'b1] = DATA[15:8];
+   end   
+endtask
+
+
+always @ (*)
+   if (DQ==118) begin
+     $display ("SUM  = ", MEM[12'h12]);
+     $finish;
+     end 
+   
+
+
+initial
+  begin
+     //FIRST WRITE THE DATA
+     wr_addr = 12'h10;     
+     WR_MEM(12'd1);
+     wr_addr = 12'h11;     
+     WR_MEM(12'd2);     
+
+     //NOW WRITE THE PROGRAM
+
+     wr_addr = 12'd0;     
+//   `define LXI_H	8'b00100001     
+     WR_MEM(8'b00100001);   //33 LXI_H HL POINTS TO ...
+//    `define LDA		8'b00111010
+//     WR_MEM(8'b00111010); // 58 MOV_A_M GET OPERAND
+
+     
+     wr_addr = 12'd1;
+     WR_MEM(8'h10);   //16 HIGH BITS OF DATA
+     wr_addr = 12'd2;
+     WR_MEM(8'h00);   //0 LOW BITS OF DATA
+
+
+
+     wr_addr = 12'd3;          
+//   `define MOV_A_M 8'b01111110
+     WR_MEM(8'b01111110); // 126 MOV_A_M GET OPERAND
+
+     
+ // `define INX_H	8'b00100011        
+     wr_addr = 12'd4;
+     WR_MEM(8'b00100011);   //35 INX_H INCREMENT
+
+//`    define ADD_M	8'b10000110     
+     wr_addr = 12'd5;
+     WR_MEM(8'b10000110);   //134 ADD_M
+     
+ // `define INX_H	8'b00100011        
+     wr_addr = 12'd6;
+     WR_MEM(8'b00100011);   //35 INX_H INCREMENT    
+
+//   `define MOV_M_A 8'b01110111     
+     wr_addr = 12'd7;
+     WR_MEM(8'b01110111); //119 MOV_M_A STORE
+     
+//   `define HLT		8'b01110110     
+     wr_addr = 12'd8;
+     WR_MEM(8'b01110110);     //118 HLT HALT
+
+     
+     $display ("OP1  = ", MEM[12'h10]);
+     $display ("OP2  = ", MEM[12'h11]);
+
+  end // initial begin
+
+
+/*         
+initial begin
+   $display("A     nOE nWE nCS Show Result DQ");
+   
+   $monitor( A, "  ",  
+             nOE,  "   ",
+             nWE, "   ",
+             nCS, "   ",
+	     show, "   ",
+	     result, "    ",
+             DQ); 
+end
+*/
+   
+endmodule
+//PROGRAM FROM //PROGRAM FROM
+//https://www.tutorialspoint.com/microprocessor/microprocessor_8085_instruction_sets.htm
